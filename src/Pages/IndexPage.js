dc17ea9ca603c4479aa41972425cc02db1b995fe
Yacdany <jack20-21@hotmail.com>
2021-11-26 11:54:43 -0600

    making a request to the pokemon API anf getting pokemons info

--- src/Pages/IndexPage.js
+++ src/Pages/IndexPage.js
@@ -1,12 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import NavBar from '../Components/NavBar/NavBar'
-
+import { Row, Container } from 'react-bootstrap'
 
 export const IndexPage = () => {
+    const [pokemon, setPokemons] = useState([])
+
+    useEffect(() => {
+        const getPokemons = async (url) => {
+            let res = await fetch(url),
+                json = await res.json();
+            //console.log(json);
+
+            json.results.forEach(async (el) => {
+                let res = await fetch(el.url),
+                    json = await res.json();
+
+                console.log(json);
+                let pokemon = {
+                    id: json.id,
+                    name: json.name,
+                    avatar: json.sprites.front_default,
+                };
+
+                setPokemons((pokemons) => [...pokemons, pokemon]);
+            });
+        };
+
+        getPokemons("https://pokeapi.co/api/v2/pokemon/");
+    }, []);
+
+
     return (
-       /*  <Container> */
+        <>
             <NavBar />
-      /*   </Container> */
+            <Container>
+                <Row>
+
+                </Row>
+            </Container>
+        </>
     )
 }
 
