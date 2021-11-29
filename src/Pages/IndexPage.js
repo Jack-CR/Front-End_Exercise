8d839fe7a410cd49dca6fbad58ad0eb6b5d308e3
Yacdany <jack20-21@hotmail.com>
2021-11-28 18:09:49 -0600

    fixing request

--- src/Pages/IndexPage.js
+++ src/Pages/IndexPage.js
@@ -10,27 +10,31 @@ export const IndexPage = () => {
   /* Imake the request to the API */
   useEffect(() => {
     const getPokemons = async (url) => {
-      let res = await fetch(url),
-        json = await res.json();
-
-      json.results.forEach(async (el) => {
-        let res = await fetch(el.url),
+      try {
+        let res = await fetch(url),
           json = await res.json();
 
+        json.results.forEach(async (el) => {
+          let res = await fetch(el.url),
+            json = await res.json();
+
           /* Saving each result "name,stats,moves etc" from the API in a single object "pokemon" */
-        let pokemon = {
-          id: json.id,
-          name: json.name,
-          avatar: json.sprites.front_default,
-          base_experience: json.base_experience,
-          weight: json.weight,
-          stats: json.stats,
-          sprites: json.sprites,
-          abilities: json.abilities,
-        };
-      
-        setPokemons((pokemons) => [...pokemons, pokemon]);
-      });
+          let pokemon = {
+            id: json.id,
+            name: json.name,
+            avatar: json.sprites.front_default,
+            base_experience: json.base_experience,
+            weight: json.weight,
+            stats: json.stats,
+            sprites: json.sprites,
+            abilities: json.abilities,
+          };
+
+          setPokemons((pokemons) => [...pokemons, pokemon]);
+        });
+      } catch (error) {
+        console.log(error);
+      }
     };
 
     getPokemons("https://pokeapi.co/api/v2/pokemon/");
@@ -43,7 +47,7 @@ export const IndexPage = () => {
         <Row className="d-flex justify-content-center mt-5">
           <TableComponent pokemons={pokemons} />
         </Row>
-        <FooterComponent /> 
+        <FooterComponent />
       </Container>
     </>
   );
